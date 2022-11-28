d5a9cde61774ebe37a43fa4ef97a1211f0124456
Martin Palkovic <martin.palkovic@cookeaqua.com>
2022-11-28 08:53:11 -0400

    updated variable names

--- SQL/Snowflake_Shorten_Huge_Union_Queries.sql
+++ SQL/Snowflake_Shorten_Huge_Union_Queries.sql
@@ -2,7 +2,7 @@
 -- Title: How to shorten a huge union query
 -- By: Martin Palkovic
 -- Date: 2022-11-25
--- Description: Have you encountered a production SQL query with a large number of unions,
+-- Description: Have you encountered a production small_sql query with a large number of unions,
 -- and very little changes between the queries except perhaps the database and/or schema name?
 -- In this example, you can loop over the COMPANY_NAME field in MY_TABLE to create
 -- one select statement per 'COMPANY_NAME', union them together, and return the results
@@ -13,16 +13,17 @@ use role sysadmin;
 use warehouse my_wh;
 use database my_db;
 
+-- Declare variables, loop over results of the 'organization' cursor variable
 declare
-    sql varchar;
-    final_sql varchar;
+    small_sql varchar;
+    big_sql varchar;
     organization cursor for (select COMPANY_NAME from DEV.MY_SCHEMA.MY_TABLE);
     my_results resultset;
 begin
-    final_sql := '';
-    
+    big_sql := '';
+    -- In Snowflake, $$ is a multi-line string delimiter 
     for company in organization do
-        sql := $$select 'COMPANY_NAME' as Company
+        small_sql := $$select 'COMPANY_NAME' as Company
             , GL.ACTNUM as Account_Number
             , ACT.DESCRIPTION as Account_Name
             from COMPANY_NAME.General_Ledger_Table GL 
@@ -30,15 +31,15 @@ begin
             inner join COMPANY_NAME.Account_Name_Table ACT
                 on ACT.ID = GL.ID
             $$;
-        sql := replace(sql, 'COMPANY_NAME', company.COMPANY_NAME);
+        small_sql := replace(small_sql, 'COMPANY_NAME', company.COMPANY_NAME);
 
-        if(final_sql != '')then 
-          final_sql := final_sql || ' union all ';
+        if(big_sql != '') then 
+          big_sql := big_sql || ' union all ';
         end if;
 
-        final_sql := final_sql || sql;
+        big_sql := big_sql || small_sql;
     end for;
     
-    my_results := (execute immediate :final_sql);
+    my_results := (execute immediate :big_sql);
     return table(my_results);
 end;
