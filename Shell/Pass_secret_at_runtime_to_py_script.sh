9d55d8db3fec4cc6dc651b8143f3a3f99d61ffb5
Martin Palkovic <martin.palkovic@cookeaqua.com>
2022-12-22 11:35:45 -0400

    updated the secrets file

--- Shell/Pass_secret_at_runtime_to_py_script.sh
+++ Shell/Pass_secret_at_runtime_to_py_script.sh
@@ -1,4 +1,4 @@
-& {Set-Item Env:my_password  "yoUr_str0Ng_paSswoRd_heRe"}; python myscript.py
+& {Set-Item Env:my_password  "yoUr_str0Ng_paSswoRd_heRe"} | py myscript.py
 
 # Your Python script must contain the following:
 # import os
